b5db211ec0a890415b310ef4df9df68784e91e3e
Gonçalo Jordão <grtjordao782001@gmail.com>
2020-06-15 18:32:11 +0100

    PL5

--- PL's/PL5 Exercícios/PL5_Exercicio_1/src/PL5Exercico_1.java
+++ PL's/PL5 Exercícios/PL5_Exercicio_1/src/PL5Exercico_1.java
@@ -1,14 +1,6 @@
-
 import java.util.Scanner;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
- *
  * @author Jordão
  */
 public class PL5Exercico_1 {
@@ -17,35 +9,30 @@ public class PL5Exercico_1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Scanner tec = new Scanner (System.in);
-        
-        int digito,andar,n,a,digitoAnterior = 0,b=1;
-        
-        n= tec.nextInt();
-        
-        
-        
-        for (a=0;a<n;a++){
+        Scanner tec = new Scanner(System.in);
+
+        int digito, andar, n, a, digitoAnterior = 0, b = 1;
+
+        System.out.print("Digite um número: ");
+        n = tec.nextInt();
+
+        for (a = 0; a < n; a++) {
             andar = tec.nextInt();
             digito = digito(andar);
-            if (digito!=digitoAnterior){
+            if (digito != digitoAnterior) {
                 b++;
             }
             digitoAnterior = digito;
         }
         System.out.println(b);
     }
-        
-        
-     
 
-        public static int digito (int codigo){
-         int digito;
-         
-         digito= codigo/10;
-         
-         return digito;
-     }
-    
+    public static int digito(int codigo) {
+        int digito;
+
+        digito = codigo / 10;
+
+        return digito;
+    }
 }
         
