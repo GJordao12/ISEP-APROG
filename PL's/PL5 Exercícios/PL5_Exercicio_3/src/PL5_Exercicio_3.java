f114bcabae154476bbe3957d6260ea5cc1d71c11
Gonçalo Jordão <grtjordao782001@gmail.com>
2020-06-15 18:40:30 +0100

    PL5 Exercicio 3

--- PL's/PL5 Exercícios/PL5_Exercicio_3/src/PL5_Exercicio_3.java
+++ PL's/PL5 Exercícios/PL5_Exercicio_3/src/PL5_Exercicio_3.java
@@ -1,14 +1,6 @@
-
 import java.util.Scanner;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
- *
  * @author Jordão
  */
 public class PL5_Exercicio_3 {
@@ -17,34 +9,34 @@ public class PL5_Exercicio_3 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Scanner tec = new Scanner (System.in);
-        
-        int num,decimal,digito,a=0,b;
-        
-        num= tec.nextInt();
-        
-        while (a!=0){
-            a=num(num);
+        Scanner tec = new Scanner(System.in);
+
+        int num, decimal, digito, a = 0, b;
+
+        num = tec.nextInt();
+
+        while (a != 0) {
+            a = num(num);
         }
     }
-    
-   public static int num (int decimal){
-       
-        int b=0,c=0,digito,a=0;
-        if (decimal>0){
-            while(decimal!=0){
-                digito=decimal%10;
+
+    public static int num(int decimal) {
+
+        int b = 0, c = 0, digito, a = 0;
+        if (decimal > 0) {
+            while (decimal != 0) {
+                digito = decimal % 10;
                 b++;
-                if (digito!=9 && digito!=8){
-                   c++;
+                if (digito != 9 && digito != 8) {
+                    c++;
                 }
-                decimal=decimal/10;
-            }    
-            if(b!=c){
+                decimal = decimal / 10;
+            }
+            if (b != c) {
                 a++;
             }
         }
-       
- }}
+    }
+}
     
 
